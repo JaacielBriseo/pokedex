eda5744b997a8ec1c708bc4af12a63934f451169
JaacielBriseo <jaacielbris23@gmail.com>
2023-03-22 18:56:46 -0700

    Pokedex ISR/ISG

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,10 +2,14 @@ import { pokeApi } from '../api';
 import { Pokemon } from '../interfaces';
 
 export const getPokemonInfo = async (query: string) => {
-	const { data } = await pokeApi.get<Pokemon>(`/pokemon/${query}`);
-	return {
-		id: data.id,
-		name: data.name,
-		sprites: data.sprites,
-	};
+	try {
+		const { data } = await pokeApi.get<Pokemon>(`/pokemon/${query}`);
+		return {
+			id: data.id,
+			name: data.name,
+			sprites: data.sprites,
+		};
+	} catch (error) {
+		return null;
+	}
 };
